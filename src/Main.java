d3dceee3a39d22cc1d3eb455781f39ca27dc24f9
Ulises Andrade <ulises.ag03@gmail.com>
2025-01-17 23:14:47 -0600

    jus for test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Mod for test in github");
         System.out.println("//////////// WELCOME TO HANGMAN GAME!!!!! /////////////");
         Hangman hangman = new Hangman();
         hangman.play();
